89269c34dbb1929f4f2011fa5ae3e3e9a9c0f2ae
liesegang <yyama0704@gmail.com>
2024-02-28 00:43:51 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coc7e_combat_simulator',
+    version="0.0.1",
+    description="combat simulator for the Call of Cthulhu 7th Edition table talk role-playing game",
+    author='liesegang',
+    packages=find_packages(),
+    license='MIT'
+)

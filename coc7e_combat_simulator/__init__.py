50f06ecb71cd6feb6934e937ca9f23906ddcca83
liesegang <yyama0704@gmail.com>
2024-02-28 01:07:19 +0900

    Update pip package related settings

---
+++ coc7e_combat_simulator/__init__.py
@@ -0,0 +1,8 @@
+__copyright__    = 'Copyright (C) 2024 liesegang'
+__version__      = '0.0.1'
+__license__      = 'MIT'
+__author__       = 'liesegang'
+__author_email__ = 'yyama0704@gmail.com'
+__url__          = 'https://github.com/Liesegang/coc_7e_combat_simulator'
+
+__all__ = []

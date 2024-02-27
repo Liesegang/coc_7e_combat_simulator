f30cb7f5608ee13f5331b59448917f01ffc3ecf6
liesegang <yyama0704@gmail.com>
2024-02-27 21:23:39 +0900

    Fix type circular dependency issue

--- coc7e_combat_simulator/strategies/reaction.py
+++ coc7e_combat_simulator/strategies/reaction.py
@@ -1,22 +1,26 @@
 from enum import Enum
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..character import Character
+
 class ReactionType(Enum):
     NOTHING = 0
     DODGE = 1
     FIGHT_BACK = 2
 
 class ReactionStrategy:
-    def reply(self, character: "Character", attacker: "Character") -> ReactionType:
+    def reply(self, character: Character, attacker: Character) -> ReactionType:
         raise NotImplementedError()
 
 class NothingReactionStrategy(ReactionStrategy):
-    def reply(self, character: "Character", attacker: "Character") -> ReactionType:
+    def reply(self, character: Character, attacker: Character) -> ReactionType:
         return ReactionType.NOTHING
 
 class DodgeReactionStrategy(ReactionStrategy):
-    def reply(self, character: "Character", attacker: "Character") -> ReactionType:
+    def reply(self, character: Character, attacker: Character) -> ReactionType:
         return ReactionType.DODGE
 
 class FightBackReactionStrategy(ReactionStrategy):
-    def reply(self, character: "Character", attacker: "Character") -> ReactionType:
+    def reply(self, character: Character, attacker: Character) -> ReactionType:
         return ReactionType.FIGHT_BACK
--- coc7e_combat_simulator/strategies/skill_selection.py
+++ coc7e_combat_simulator/strategies/skill_selection.py
@@ -3,16 +3,20 @@ import random
 from ..skill import Skill
 from ..dice_parser import DiceParser
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..character import Character
+
 class SkillSelectionStrategy:
-    def select_skill(self, character: "Character") -> Skill:
+    def select_skill(self, character: Character) -> Skill:
         raise NotImplementedError()
 
 class RandomSkillSelectionStrategy(SkillSelectionStrategy):
-    def select_skill(self, character: "Character") -> Skill:
+    def select_skill(self, character: Character) -> Skill:
         return random.choice(character.skills)
 
 class ExpectedDamageMaximizationSkillSelectionStrategy(SkillSelectionStrategy):
     dice_parser = DiceParser()
 
-    def select_skill(self, character: "Character") -> Skill:
+    def select_skill(self, character: Character) -> Skill:
         return max(character.skills, key=lambda skill: skill.success_rate / 100 * ExpectedDamageMaximizationSkillSelectionStrategy.dice_parser.expected(skill.damage))
